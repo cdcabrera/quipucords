0308145d600e05831960279c9be0df8b83636d3f
CD Cabrera <ccabrera@redhat.com>
2018-01-16 15:56:25 -0500

    UI Redux and API interaction
    Expose sources, facts, reports, and credentials endpoint
    methods.

--- client/src/services/credentialsApi.js
+++ client/src/services/credentialsApi.js
@@ -1,6 +1,60 @@
+import jquery from 'jquery';
+
 class CredentialsApi {
-  static getCredentials() {
-    return fetch(process.env.REACT_APP_CREDENTIALS_API).then(response => {
+  static addCredential(data = {}) {
+    return fetch(process.env.REACT_APP_CREDENTIALS_API, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      })
+    }).then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error(response.statusText);
+      }
+    });
+  }
+
+  static deleteCredential(id) {
+    return fetch(`${process.env.REACT_APP_CREDENTIALS_API}${id}`, {
+      method: 'DELETE'
+    }).then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error(response.statusText);
+      }
+    });
+  }
+
+  static getCredential(id) {
+    return this.getCredentials(id);
+  }
+
+  static getCredentials(id = '', query = {}) {
+    let queryStr = jquery.param(query);
+
+    return fetch(
+      `${process.env.REACT_APP_CREDENTIALS_API}${id}${queryStr}`
+    ).then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error(response.statusText);
+      }
+    });
+  }
+
+  static updateCredential(id, data = {}) {
+    return fetch(`${process.env.REACT_APP_CREDENTIALS_API}${id}`, {
+      method: 'PUT',
+      body: JSON.stringify(data),
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      })
+    }).then(response => {
       if (response.ok) {
         return response.json();
       } else {
--- client/src/services/sourcesApi.js
+++ client/src/services/sourcesApi.js
@@ -1,6 +1,60 @@
+import jquery from 'jquery';
+
 class SourcesApi {
-  static getSources() {
-    return fetch(process.env.REACT_APP_SOURCES_API).then(response => {
+  static addSource(data = {}) {
+    return fetch(process.env.REACT_APP_SOURCES_API, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      })
+    }).then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error(response.statusText);
+      }
+    });
+  }
+
+  static deleteSource(id) {
+    return fetch(`${process.env.REACT_APP_SOURCES_API}${id}`, {
+      method: 'DELETE'
+    }).then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error(response.statusText);
+      }
+    });
+  }
+
+  static getSource(id) {
+    return this.getSources(id);
+  }
+
+  static getSources(id = '', query = {}) {
+    let queryStr = jquery.param(query);
+
+    return fetch(`${process.env.REACT_APP_SOURCES_API}${id}${queryStr}`).then(
+      response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error(response.statusText);
+        }
+      }
+    );
+  }
+
+  static updateSource(id, data = {}) {
+    return fetch(`${process.env.REACT_APP_SOURCES_API}${id}`, {
+      method: 'PUT',
+      body: JSON.stringify(data),
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      })
+    }).then(response => {
       if (response.ok) {
         return response.json();
       } else {
