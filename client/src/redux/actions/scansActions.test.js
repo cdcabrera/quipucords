1f963f74821f087c17f7e1d391849f35e29154ff
CD Cabrera <ccabrera@redhat.com>
2018-01-17 13:18:56 -0500

    UI Action and reducer unit tests
    Package updates for testing with Create React App. Initial
    action and reducer unit tests

---
+++ client/src/redux/actions/scansActions.test.js
@@ -0,0 +1,35 @@
+import * as types from '../constants/scansConstants';
+import * as actions from './scansActions';
+
+describe('ScansActions', function() {
+  it('should create a GET error action', () => {
+    const error = true;
+    const expectedAction = {
+      type: types.GET_SCANS_ERROR,
+      error,
+      message: undefined
+    };
+
+    expect(actions.getScansError(error)).toEqual(expectedAction);
+  });
+
+  it('should create a GET loading action', () => {
+    const loading = true;
+    const expectedAction = {
+      type: types.GET_SCANS_LOADING,
+      loading
+    };
+
+    expect(actions.getScansLoading(loading)).toEqual(expectedAction);
+  });
+
+  it('should create a GET success action', () => {
+    const data = {};
+    const expectedAction = {
+      type: types.GET_SCANS_SUCCESS,
+      data
+    };
+
+    expect(actions.getScansSuccess(data)).toEqual(expectedAction);
+  });
+});
