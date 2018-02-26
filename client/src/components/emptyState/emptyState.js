152743ca9c38fb62160c246e81d8e66d54a32615
CD Cabrera <ccabrera@redhat.com>
2018-02-26 16:45:36 -0500

    DEV Testing single Empty State component

---
+++ client/src/components/emptyState/emptyState.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Button, DropdownButton, EmptyState, Grid, MenuItem, Row } from 'patternfly-react';
+
+const DefaultEmptyState = ({ onAddCredential, onAddSource }) => {
+  return (
+    <Grid fluid>
+      <Row>
+        <EmptyState className="full-page-blank-slate">
+          <EmptyState.Icon />
+          <EmptyState.Title>Welcome to Red Hat Entitlements Reporting</EmptyState.Title>
+
+          {onAddCredential &&
+            onAddSource && (
+              <React.Fragment>
+                <EmptyState.Info>
+                  A credential defines a set of user authentication information to be used during a scan.<br />A
+                  credential includes a username and a password or SSH key. Entitlements Reporting uses SSH<br />to
+                  connect to servers on the network and uses credentials to access those servers.
+                </EmptyState.Info>
+                <EmptyState.Action>
+                  <DropdownButton
+                    bsStyle="primary"
+                    bsSize="large"
+                    title="Add Credential"
+                    pullRight
+                    id="createCredentialButton"
+                  >
+                    <MenuItem eventKey="1" onClick={() => onAddCredential('network')}>
+                      Network Credential
+                    </MenuItem>
+                    <MenuItem eventKey="2" onClick={() => onAddCredential('satellite')}>
+                      Satellite Credential
+                    </MenuItem>
+                    <MenuItem eventKey="2" onClick={() => onAddCredential('vcenter')}>
+                      VCenter Credential
+                    </MenuItem>
+                  </DropdownButton>
+                </EmptyState.Action>
+                <EmptyState.Action secondary>
+                  <Button bsStyle="default" onClick={onAddSource}>
+                    Add Source
+                  </Button>
+                </EmptyState.Action>
+              </React.Fragment>
+            )}
+
+          {!onAddCredential &&
+            onAddSource && (
+              <React.Fragment>
+                <EmptyState.Info>
+                  A source defines a collection of network information, including IP addresses or host names,<br /> or
+                  systems management solution information, in addition to SSH ports and SSH credentials
+                </EmptyState.Info>
+                <EmptyState.Action>
+                  <Button bsStyle="primary" bsSize="large" onClick={onAddSource}>
+                    Add Source
+                  </Button>
+                </EmptyState.Action>
+              </React.Fragment>
+            )}
+        </EmptyState>
+      </Row>
+    </Grid>
+  );
+};
+
+DefaultEmptyState.propTypes = {
+  onAddCredential: PropTypes.func,
+  onAddSource: PropTypes.func
+};
+
+export default DefaultEmptyState;
