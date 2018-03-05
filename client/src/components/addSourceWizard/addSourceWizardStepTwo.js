068340bfded3ec6c8b953e1e11f0a38b7880334e
CD Cabrera <ccabrera@redhat.com>
2018-03-05 10:29:22 -0500

    UI: Add source wizard updates
    * Port placeholder copy updated

--- client/src/components/addSourceWizard/addSourceWizardStepTwo.js
+++ client/src/components/addSourceWizard/addSourceWizardStepTwo.js
@@ -295,7 +295,7 @@ class AddSourceWizardStepTwo extends React.Component {
                 name="port"
                 type="text"
                 value={port}
-                placeholder="Enter a port"
+                placeholder="Default port :22"
                 onChange={this.onChangePort}
               />
             </FieldGroup>
@@ -311,7 +311,7 @@ class AddSourceWizardStepTwo extends React.Component {
                 name="hosts"
                 type="text"
                 value={singleHostPort}
-                placeholder="Enter an IP address:port"
+                placeholder="Enter an IP address (default port :443)"
                 onChange={this.onChangeHost}
               />
             </FieldGroup>
