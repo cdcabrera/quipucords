d5d9d16b96b301d2a1a16ccfa22c283d76654cd6
CD Cabrera <ccabrera@redhat.com>
2018-09-25 03:26:27 -0400

    DEV simpleTooltip component
    * linter, simpleTooltip component

--- client/src/components/simpleTooltIp/simpleTooltip.js
+++ client/src/components/simpleTooltIp/simpleTooltip.js
@@ -2,22 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { OverlayTrigger, Tooltip } from 'patternfly-react';
 
-const SimpleTooltip = ({ children, tooltip, placement, trigger, delayShow, ...props }) => {
-  return (
-    <OverlayTrigger
-      overlay={<Tooltip {...props}>{tooltip}</Tooltip>}
-      placement={placement}
-      trigger={trigger}
-      delayShow={delayShow}
-    >
-      <span>{children}</span>
-    </OverlayTrigger>
-  );
-};
+const SimpleTooltip = ({ children, tooltip, placement, trigger, delayShow, ...props }) => (
+  <OverlayTrigger
+    overlay={<Tooltip {...props}>{tooltip}</Tooltip>}
+    placement={placement}
+    trigger={trigger}
+    delayShow={delayShow}
+  >
+    <span>{children}</span>
+  </OverlayTrigger>
+);
 
 SimpleTooltip.propTypes = {
-  children: PropTypes.node,
-  tooltip: PropTypes.node,
+  children: PropTypes.node.isRequired,
+  tooltip: PropTypes.node.isRequired,
   placement: PropTypes.string,
   trigger: PropTypes.array,
   delayShow: PropTypes.number
