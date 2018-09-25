66a22bcb22362229c7df647f69e58b9332d934bd
CD Cabrera <ccabrera@redhat.com>
2018-09-25 03:34:27 -0400

    DEV content component
    * linter, content component

--- client/src/components/content/content.js
+++ client/src/components/content/content.js
@@ -3,23 +3,23 @@ import { Redirect, Route, Switch } from 'react-router-dom';
 import { routes } from '../../routes';
 
 class Content extends React.Component {
-  renderRoutes() {
+  static renderRoutes() {
     let redirectRoot = null;
 
     return {
-      renderRoutes: routes().map((item, index) => {
+      renderRoutes: routes().map(item => {
         if (item.redirect === true) {
           redirectRoot = <Redirect from="/" to={item.to} />;
         }
 
-        return <Route key={index} path={item.to} component={item.component} />;
+        return <Route key={item.to} path={item.to} component={item.component} />;
       }),
       redirectRoot
     };
   }
 
   render() {
-    const { renderRoutes, redirectRoot } = this.renderRoutes();
+    const { renderRoutes, redirectRoot } = Content.renderRoutes();
 
     return (
       <div className="quipucords-content">
