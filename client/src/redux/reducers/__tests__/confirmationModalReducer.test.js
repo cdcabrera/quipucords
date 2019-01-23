93427ccbb3b23f140a0a8d04088aa40a36642a67
CD Cabrera <ccabrera@redhat.com>
2019-01-23 17:06:10 -0500

    DEV confirmationModal reducer test

--- client/src/redux/reducers/__tests__/confirmationModalReducer.test.js
+++ client/src/redux/reducers/__tests__/confirmationModalReducer.test.js
@@ -6,15 +6,17 @@ describe('ConfirmationModalReducer', () => {
     expect(confirmationModalReducer.initialState).toBeDefined();
   });
 
-  it('should handle all defined types', () => {
-    Object.keys(types).forEach(value => {
+  it('should handle specific defined types', () => {
+    const specificTypes = [types.CONFIRMATION_MODAL_HIDE, types.CONFIRMATION_MODAL_SHOW];
+
+    specificTypes.forEach(value => {
       const dispatched = {
         type: value
       };
 
       const resultState = confirmationModalReducer(undefined, dispatched);
 
-      expect({ type: value, result: resultState }).toMatchSnapshot(`defined types ${value}`);
+      expect({ type: value, result: resultState }).toMatchSnapshot(`defined type ${value}`);
     });
   });
 });
